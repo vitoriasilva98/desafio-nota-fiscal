ca2193cdd7d4b0416d87b72c7577a35431d3d07e
Vitória Vitoria <vitoriasilva98@hotmail.com>
2025-04-06 21:29:28 -0300

    Docs - Adicionando dependência para documentação + anotações

--- geradornotafiscal 2/src/main/java/br/com/itau/geradornotafiscal/model/Endereco.java
+++ geradornotafiscal 2/src/main/java/br/com/itau/geradornotafiscal/model/Endereco.java
@@ -3,6 +3,7 @@ package br.com.itau.geradornotafiscal.model;
 import br.com.itau.geradornotafiscal.enums.Finalidade;
 import br.com.itau.geradornotafiscal.enums.Regiao;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,24 +16,32 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Endereco {
+
+    @Schema(description = "Código de Endereçamento Postal (CEP)", example = "03105003")
     @JsonProperty("cep")
     private String cep;
 
+    @Schema(description = "Logradouro", example = "Avenida Paulista")
     @JsonProperty("logradouro")
     private String logradouro;
 
+    @Schema(description = "Numeração do logradouro", example = "52")
     @JsonProperty("numero")
     private String numero;
 
+    @Schema(description = "Estado", example = "SP")
     @JsonProperty("estado")
     private String estado;
 
+    @Schema(description = "Complemento do endereço", example = "bloco 5, sala 500")
     @JsonProperty("complemento")
     private String complemento;
 
+    @Schema(description = "Finalidade do endereço", example = "ENTREGA")
     @JsonProperty("finalidade")
     private Finalidade finalidade;
 
+    @Schema(description = "Região", example = "SUDESTE")
     @JsonProperty("regiao")
     private Regiao regiao;
 
--- geradornotafiscal 2/src/main/java/br/com/itau/geradornotafiscal/web/controller/GeradorNFController.java
+++ geradornotafiscal 2/src/main/java/br/com/itau/geradornotafiscal/web/controller/GeradorNFController.java
@@ -1,6 +1,9 @@
 package br.com.itau.geradornotafiscal.web.controller;
 
 import br.com.itau.geradornotafiscal.model.NotaFiscal;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 import br.com.itau.geradornotafiscal.model.Pedido;
@@ -12,18 +15,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Notas Fiscais", description = "Operações relacionadas a notas fiscais")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/pedido")
 public class GeradorNFController {
 
-	private final IGeradorNotaFiscalService notaFiscalService;
+    private final IGeradorNotaFiscalService notaFiscalService;
 
-	@PostMapping("/gerarNotaFiscal")
-	public ResponseEntity<NotaFiscal> gerarNotaFiscal(@RequestBody Pedido pedido) {
-		return ResponseEntity
-				.status(HttpStatus.CREATED)
-				.body(notaFiscalService.gerarNotaFiscal(pedido));
-	}
-	
+    @Operation(summary = "Gera uma nova nota fiscal")
+    @ApiResponse(responseCode = "201", description = "Nota fiscal criada com sucesso")
+    @PostMapping("/gerarNotaFiscal")
+    public ResponseEntity<NotaFiscal> gerarNotaFiscal(@RequestBody Pedido pedido) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(notaFiscalService.gerarNotaFiscal(pedido));
+    }
+
 }
