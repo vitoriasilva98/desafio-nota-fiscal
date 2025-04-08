1747ba840325005b6e10bd5c2f5aebaade95ae05
Vitória Vitoria <vitoriasilva98@hotmail.com>
2025-04-08 14:42:07 -0300

    Performance - Remoção dos modificadores de acesso das classes de teste

--- geradornotafiscal/src/test/java/br/com/itau/geradornotafiscal/model/DestinatarioTest.java
+++ geradornotafiscal/src/test/java/br/com/itau/geradornotafiscal/model/DestinatarioTest.java
@@ -17,7 +17,7 @@ import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
-public class DestinatarioTest {
+class DestinatarioTest {
 
     @Mock
     private Endereco enderecoEntrega;
